f2df0df3cd5d98c82146ee2047b322eaa304e984
Sajjad1996 <sajjad75926@gmail.com>
2024-04-11 19:01:51 +0330

    Debugging methods.

--- src/app/components/App.test.tsx
+++ src/app/components/App.test.tsx
@@ -1,10 +1,16 @@
-import {render, screen} from "@testing-library/react";
+import {logRoles, render, screen} from "@testing-library/react";
 import App from "@/app/components/App";
 
 describe('Login Component', () => {
     it('Renders based on authenticated user.', async () => {
-        render(<App/>)
-        const signOutButtonElement = await screen.findByRole('button', {name: 'SignOut'}, {timeout: 2000}); //default is 1000ms
+        const view = render(<App/>)
+        screen.debug(view.container);
+        // logRoles(view.container);
+        // screen.logTestingPlaygroundURL(view.container)
+
+        const signOutButtonElement = await screen.findByRole('button', {name: 'SignOut'}, /*{timeout: 2000}*/);
+        screen.debug(view.container);
+        // logRoles(view.container);
         expect(signOutButtonElement).toBeInTheDocument();
     });
 })
